4c5480a557da5c2d652972b1cfebc0f6b0ad5f08
Jia Zhang <facade1801@163.com>
2019-11-03 17:41:53 -0600

    Added empty navigation
    Can be accessed in Main Activity by a button.

--- app/src/main/java/com/example/upeers/MainActivity.kt
+++ app/src/main/java/com/example/upeers/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.upeers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClickTemporaryButton(view: View) {
+        val intent = Intent(this, MainNaviActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+
 }
