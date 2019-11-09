94aca5837552aeea571f1197f74ddd5b34ef321e
Jia Zhang <facade1801@163.com>
2019-11-09 13:05:31 -0600

    Added a dummy button on MainActivity (Nothing special)

--- app/src/main/java/com/example/upeers/MainActivity.kt
+++ app/src/main/java/com/example/upeers/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.upeers.coursepage.CoursePageActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +19,10 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun onClickTemporaryButton2(view: View) {
+        val intent = Intent(this, CoursePageActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
 
 }
---
+++ app/src/main/java/com/example/upeers/coursepage/CoursePageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.upeers.coursepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.upeers.R
+
+class CoursePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_page)
+    }
+}
