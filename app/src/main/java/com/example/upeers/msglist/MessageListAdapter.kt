1fb0fa83f32e8e195b10e590243d7e9d99e164e7
leosuperbugs <tnie2@wisc.edu>
2019-11-10 16:06:50 -0600

    gitignore

---
+++ app/src/main/java/com/example/upeers/msglist/MessageListAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.upeers.msglist
+
+import android.content.Context
+
+class MessageListAdapter(private val mContext:Context,
+                         private val mMessageList: List<Message>) {
+}
---
+++ app/src/main/java/com/example/upeers/msglist/MessagesAndUser.kt
@@ -0,0 +1,11 @@
+package com.example.upeers.msglist
+
+class Message(val message: String,
+              val sender: User,
+              val createdAt: Long) {
+
+}
+
+class User (val nickname: String, val porfileUrl: String) {
+
+}
