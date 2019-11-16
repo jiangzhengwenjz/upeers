e247d9c89e99ff2e11641e40ff536cb382028e3a
leosuperbugs <tnie2@wisc.edu>
2019-11-16 13:05:28 -0600

    customize kudos style

---
+++ app/src/main/java/com/example/upeers/tutordetail/TutorDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.upeers.tutordetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.upeers.R
+
+class TutorDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_detail)
+    }
+}
