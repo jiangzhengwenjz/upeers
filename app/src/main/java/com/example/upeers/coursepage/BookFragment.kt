17f5f48cea5f6a7976d6775d92816b499ebd4313
leosuperbugs <tnie2@wisc.edu>
2019-11-17 11:38:55 -0600

    book list finished, sorting is missing

--- app/src/main/java/com/example/upeers/coursepage/BookFragment.kt
+++ app/src/main/java/com/example/upeers/coursepage/BookFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.upeers.R
 
@@ -15,14 +17,48 @@ import com.example.upeers.R
  */
 class BookFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdaptor: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val books = listOf<BookInfo>(
+            BookInfo(
+                27,
+                "Keller Hall",
+                "Almost brand new book without any mark",
+                R.drawable.book_example4,
+                R.drawable.book_example2,
+                5),
+            BookInfo(28,
+                "Stadium Villiage",
+                "Perfect condition with a little note on pages",
+                R.drawable.book_example3,
+                R.drawable.book_example1,
+                7),
+            BookInfo(35,
+                "Northrop",
+                "My book looks like a new one",
+                R.drawable.book_example2,
+                R.drawable.book_example4,
+                0)
+        )
         val root = inflater.inflate(R.layout.fragment_book, container, false)
         val bookStdImg: ImageView = root.findViewById(R.id.book_std_img)
         bookStdImg.setImageResource(R.drawable.design_norman)
+
+        viewManager = LinearLayoutManager(activity)
+        viewAdaptor = BookListAdaptor(books, this.context)
+
+        recyclerView = root.findViewById<RecyclerView>(R.id.books_list).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdaptor
+        }
         return root
     }
 
---
+++ app/src/main/java/com/example/upeers/coursepage/BookInfo.kt
@@ -0,0 +1,12 @@
+package com.example.upeers.coursepage
+
+class BookInfo(
+    val price: Int,
+    val location: String,
+    val description: String,
+    val image_1: Int,
+    val image_2: Int,
+    val viewCount: Int
+) {
+
+}
