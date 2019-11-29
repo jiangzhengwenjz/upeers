359876ebdab6ca6cfc2f9f524e9fcba6bd95a8fb
leosuperbugs <tnie2@wisc.edu>
2019-11-29 15:36:26 -0600

    final

--- app/src/main/java/com/example/upeers/myselling/MySellingActivity.kt
+++ app/src/main/java/com/example/upeers/myselling/MySellingActivity.kt
@@ -1,9 +1,12 @@
 package com.example.upeers.myselling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import com.example.upeers.R
+import com.example.upeers.ui.book.BookDetailActivity
 
 
 class MySellingActivity : AppCompatActivity() {
@@ -15,5 +18,11 @@ class MySellingActivity : AppCompatActivity() {
         val image2 : ImageView = findViewById(R.id.book_img_2)
         image1.setImageResource(R.drawable.book_example1)
         image2.setImageResource(R.drawable.book_example2)
+    }
+
+    fun onClickBook(view: View) {
+        val intent = Intent(this, BookDetailActivity::class.java).apply {}
+
+        startActivity(intent)
     }
 }
